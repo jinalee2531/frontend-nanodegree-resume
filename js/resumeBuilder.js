5c023ef7bcc1e6c76411fe215366ca9931afd884
jinalee2531 <jinalee@andrew.cmu.edu>
2016-10-27 21:41:18 -0700

    Format Data, Build A Resume Header

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var formattedName = HTMLheaderName.replace("%data%", "Jina");
+ var formattedRole = HTMLheaderRole.replace("%data%", "Mind Miner")
+
+ $("#header").append(formattedName)
+  $("#header").append(formattedRole)
